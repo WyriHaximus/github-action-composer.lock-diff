2e8acc3abac95bc6d5be41bf7dbcc8572d7626f5
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-05-15 22:17:06 +0200

    Simplify and explicitly specify branches

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,27 +2,18 @@
 
 set -eo pipefail
 
-if [ $(echo ${GITHUB_BASE_REF} | wc -c) -eq 1 ] ; then
-  if git rev-parse --verify main > /dev/null 2>&1; then
-    branch="main"
-  elif git rev-parse --verify master > /dev/null 2>&1; then
-    branch="master"
-  fi
-else
-  branch="${GITHUB_BASE_REF}"
-fi
-
-echo "Found default branch: ${branch}"
+echo "Base branch: ${GITHUB_BASE_REF}"
+echo "Head branch: ${GITHUB_HEAD_REF}"
 
 git fetch --depth=1 origin +refs/heads/*:refs/heads/*
 
-/workdir/vendor/bin/composer-diff "${branch}" --with-links --with-platform --no-dev -vvv > /workdir/production.md
+/workdir/vendor/bin/composer-diff "${GITHUB_BASE_REF}:composer.lock" "${GITHUB_HEAD_REF}:composer.lock" --with-links --with-platform --no-dev -vvv > /workdir/production.md
 
 echo "Production:"
 cat /workdir/production.md
 php /workdir/comment.php production "🏰 Composer Production Dependency changes 🏰"
 
-/workdir/vendor/bin/composer-diff "${branch}" --with-links --with-platform --no-prod -vvv > /workdir/development.md
+/workdir/vendor/bin/composer-diff "${GITHUB_BASE_REF}:composer.lock" "${GITHUB_HEAD_REF}:composer.lock" --with-links --with-platform --no-prod -vvv > /workdir/development.md
 
 echo "Development:"
 cat /workdir/development.md
