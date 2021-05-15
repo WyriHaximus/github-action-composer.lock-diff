df67d256fdb477016ccdd12f5449a9a6633be60d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-05-15 22:49:01 +0200

    Allow git fetch to fail and hope for the best

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 echo "Base branch: ${GITHUB_BASE_REF}"
 echo "Head branch: ${GITHUB_HEAD_REF}"
 
-git fetch --depth=1 origin +refs/heads/*:refs/heads/*
+git fetch --depth=1 origin +refs/heads/*:refs/heads/* || true
 
 /workdir/vendor/bin/composer-diff "${GITHUB_BASE_REF}:composer.lock" "${GITHUB_HEAD_REF}:composer.lock" --with-links --with-platform --no-dev -vvv > /workdir/production.md
 
