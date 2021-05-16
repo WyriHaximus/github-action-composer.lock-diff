f5f7844cd054bef808b1cdd4810c998d34adf6bd
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-05-16 22:01:45 +0200

    Add outputs
    By adding these outputs users can decide not to let this action do the
    commenting but pass them into something else.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,21 +8,31 @@ echo "Head branch: ${GITHUB_HEAD_REF}"
 git fetch --depth=1 origin +refs/heads/*:refs/heads/* || true
 
 /workdir/vendor/bin/composer-diff "${GITHUB_BASE_REF}:composer.lock" "${GITHUB_HEAD_REF}:composer.lock" --with-links --with-platform --no-dev -vvv > /workdir/production.md
-echo "Production:"
-cat /workdir/production.md
-
-if [ "$INPUT_DRYRUN" != "yes" ]
-then
-  echo "No in dry run so upserting production comment when desirable"
-  php /workdir/comment.php production "🏰 Composer Production Dependency changes 🏰"
-fi
-
+production=$(cat /workdir/production.md)
 /workdir/vendor/bin/composer-diff "${GITHUB_BASE_REF}:composer.lock" "${GITHUB_HEAD_REF}:composer.lock" --with-links --with-platform --no-prod -vvv > /workdir/development.md
+development=$(cat /workdir/development.md)
+
+echo "Production:"
+echo "${production}"
+echo "--------------------------------------------------"
 echo "Development:"
-cat /workdir/development.md
+echo "${development}"
+
+production="${production//'%'/'%25'}"
+production="${production//$'\n'/'%0A'}"
+production="${production//$'\r'/'%0D'}"
+
+development="${development//'%'/'%25'}"
+development="${development//$'\n'/'%0A'}"
+development="${development//$'\r'/'%0D'}"
+
+echo "::set-output name=production::$production"
+echo "::set-output name=development::$development"
+
 
 if [ "$INPUT_DRYRUN" != "yes" ]
 then
-  echo "No in dry run so upserting development comment when desirable"
+  echo "No in a dry run so upserting comments when desirable"
+  php /workdir/comment.php production "🏰 Composer Production Dependency changes 🏰"
   php /workdir/comment.php development "🚧 Composer Development Dependency changes 🚧"
 fi
