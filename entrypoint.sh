c51b14564cc49990be9b75b576a176c8a54a07f6
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-05-16 12:49:50 +0200

    Add Dry Run option

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,21 @@ echo "Head branch: ${GITHUB_HEAD_REF}"
 git fetch --depth=1 origin +refs/heads/*:refs/heads/* || true
 
 /workdir/vendor/bin/composer-diff "${GITHUB_BASE_REF}:composer.lock" "${GITHUB_HEAD_REF}:composer.lock" --with-links --with-platform --no-dev -vvv > /workdir/production.md
-
 echo "Production:"
 cat /workdir/production.md
-php /workdir/comment.php production "🏰 Composer Production Dependency changes 🏰"
+
+if [ "$INPUT_DRYRUN" != "yes" ]
+then
+  echo "No in dry run so upserting production comment when desirable"
+  php /workdir/comment.php production "🏰 Composer Production Dependency changes 🏰"
+fi
 
 /workdir/vendor/bin/composer-diff "${GITHUB_BASE_REF}:composer.lock" "${GITHUB_HEAD_REF}:composer.lock" --with-links --with-platform --no-prod -vvv > /workdir/development.md
-
 echo "Development:"
 cat /workdir/development.md
-php /workdir/comment.php development "🚧 Composer Development Dependency changes 🚧"
+
+if [ "$INPUT_DRYRUN" != "yes" ]
+then
+  echo "No in dry run so upserting development comment when desirable"
+  php /workdir/comment.php development "🚧 Composer Development Dependency changes 🚧"
+fi
