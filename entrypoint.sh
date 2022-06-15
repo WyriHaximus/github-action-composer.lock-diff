60a2a3dc4465cc94df3b6c8eeee40bf7cdb329ba
Cees-Jan Kiewiet <ceesjank@gmail.com>
2022-06-15 23:14:52 +0200

    Use merge commit sha to compare against

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,19 @@ set -eo pipefail
 
 # c.f. https://github.com/actions/checkout/issues/760
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
+merged_commit_sha=$(php get-merged-commit-sha.php)
 
 echo "Base branch: ${GITHUB_BASE_REF}"
 echo "Head branch: ${GITHUB_HEAD_REF}"
+echo "Merged CSHA: ${merged_commit_sha}"
 
 git fetch --depth=1 origin +refs/heads/*:refs/heads/* || true
+curl -s -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" $(curl -s -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/${GITHUB_REPOSITORY}/git/trees/${merged_commit_sha}" | jq -r '.tree[]  | select(.path == "composer.lock") | .url') | jq -r '.content' | base64 -d > "/tmp/${merged_commit_sha}-composer.lock"
+cat "/tmp/${merged_commit_sha}-composer.lock"
 
-/workdir/vendor/bin/composer-diff "${GITHUB_BASE_REF}:composer.lock" "${GITHUB_HEAD_REF}:composer.lock" --with-links --with-platform --no-dev -vvv > /workdir/production.md
+/workdir/vendor/bin/composer-diff "${GITHUB_BASE_REF}:composer.lock" "/tmp/${merged_commit_sha}-composer.lock" --with-links --with-platform --no-dev -vvv > /workdir/production.md
 production=$(cat /workdir/production.md)
-/workdir/vendor/bin/composer-diff "${GITHUB_BASE_REF}:composer.lock" "${GITHUB_HEAD_REF}:composer.lock" --with-links --with-platform --no-prod -vvv > /workdir/development.md
+/workdir/vendor/bin/composer-diff "${GITHUB_BASE_REF}:composer.lock" "/tmp/${merged_commit_sha}-composer.lock" --with-links --with-platform --no-prod -vvv > /workdir/development.md
 development=$(cat /workdir/development.md)
 
 echo "Raw:"
